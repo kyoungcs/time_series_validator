512a9140673d11f4350606d9ea002f1faa3c14e9
Daniel Soto <sotod@sonoma.edu>
2017-03-24 16:32:27 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='time_series_validator',
+      version='0.0',
+      description='scripts for linting time series data',
+      url='',
+      author='Daniel Soto',
+      author_email='sotod@sonoma.edu',
+      license='',
+      packages=['time_series_validator'],
+      zip_safe=False)
---
+++ time_series_validator/csv_validator.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+def csv_col_is_numeric(filename, col=1, skiprows=1):
+    with open(filename) as f:
+        for line in f.readlines()[skiprows:]:
+            vals = line.strip().split(',')
+            #print(vals)
+            try:
+                x = float(vals[col])
+            except:
+                return line.strip()
+
+def csv_col_duplicates(filename, col=1, skiprows=1):
+    with open(filename) as f:
+        existing_values = []
+        for line in f.readlines()[skiprows:]:
+            vals = line.strip().split(',')
+            if vals[col] not in existing_values:
+                existing_values.append(vals[col])
+            else:
+                return line.strip()
+
+def csv_col_is_monotonic(filename, col=1, skiprows=1):
+    with open(filename) as f:
+        lines = f.readlines()[skiprows:]
+        previous_line = lines[0]
+        previous_value = previous_line.strip().split(',')[col]
+        for line in lines[skiprows+1:]:
+            current_value = line.strip().split(',')[col]
+            if float(current_value) < float(previous_value):
+                return previous_line.strip(), line.strip()
+            previous_value = current_value
+            previous_line = line
+
+def csv_col_dates_valid(filename, col=0, skiprows=1):
+    data = pd.read_csv(filename, parse_dates=True, index_col=col, skiprows=skiprows)
+    if data.index.dtype == 'O':
+        return 'invalid date'
+    else:
+        return 0
+
